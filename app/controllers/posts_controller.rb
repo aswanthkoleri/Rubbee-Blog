ffe9e41866061bb59b5d773c5b74b03b1dd94022
aswanthkoleri <aswanth9495@gmail.com>
2019-10-30 00:50:39 +0530

    Add post functions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
   def index
   	
   end
+  def new
+
+  end
+  def create
+
+  end
+  def edit
+
+  end
+  def show
+
+  end
+  def destroy
+  	
+  end
 end
