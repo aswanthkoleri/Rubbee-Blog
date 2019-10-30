d5ad297292bba21fbddb82fa05465914787c093b
aswanthkoleri <aswanth9495@gmail.com>
2019-10-30 06:46:27 +0530

    Display post from DB to the views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,18 @@
 class PostsController < ApplicationController
   def index
-  	
+  	@posts = Post.all
   end
   def new
-
+  	
   end
   def create
-
+  	
   end
   def edit
 
   end
   def show
-
+  	@post = Post.find(params[:id])
   end
   def destroy
   	
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 
 Rails.application.routes.draw do
-  get 'posts/index'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/edit'
+  # get 'categories/new'
+  # get 'posts/index'
   get '/', to: "home#index"
   resources :posts
+  resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
