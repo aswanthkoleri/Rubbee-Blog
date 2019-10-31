8508195fe9c579d0880c0f01f99a24f7430eb853
aswanthkoleri <aswanth9495@gmail.com>
2019-11-01 01:16:59 +0530

    Add create form for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,27 @@ class PostsController < ApplicationController
   def index
   	@posts = Post.all
   end
+
   def new
-  	
+  	@post = Post.new
+  	@category = Category.all
   end
+  
   def create
-  	
+  	@post = Post.new(post_params)
+  	if @post.save
+  		redirect_to posts_path
+  	else
+  		render "new"
+  	end
   end
+  
+
+
   def edit
 
   end
+
   def show
   	@post = Post.find(params[:id])
   end
@@ -18,3 +30,8 @@ class PostsController < ApplicationController
   	
   end
 end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
+  end
